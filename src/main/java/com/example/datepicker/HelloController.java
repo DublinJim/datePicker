d289de9b0c6f99d031889ec6e7a0cb9f603b7386
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-17 13:21:49 -0500

    adds text that appears when activated

--- src/main/java/com/example/datepicker/HelloController.java
+++ src/main/java/com/example/datepicker/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.datepicker;
 
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
-public class HelloController {
+public class HelloController implements Initializable {
 
     @FXML
     private DatePicker datePicker;
@@ -14,11 +16,22 @@ public class HelloController {
     @FXML
     private Text txt1;
 
+
+    @FXML
+    private Text txtLabel;
+
     @FXML
     void getTheDate() {
         LocalDate time = datePicker.getValue();
         txt1.setText(String.valueOf(time));
+        txt1.setVisible(true);
+        txtLabel.setVisible(true);
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        txtLabel.setVisible(false);
+        txt1.setVisible(false);
+    }
 }
